71c0f660c623af68ac9cc7e2be2b00ff86335134
Ed Merks <ed.merks@gmail.com>
2024-10-01 16:59:31 +0200

    Remove <resolver>p2</resolver> from target-platform-configuration

--- plugins/org.eclipse.justj.codegen/src/org/eclipse/justj/codegen/templates/fragment/POMXML.java
+++ plugins/org.eclipse.justj.codegen/src/org/eclipse/justj/codegen/templates/fragment/POMXML.java
@@ -66,15 +66,14 @@ public class POMXML
   protected static final String _40 = "<plugins>";
   protected static final String _41 = "<project";
   protected static final String _42 = "<relativePath>../../releng/";
-  protected static final String _43 = "<resolver>p2</resolver>";
-  protected static final String _44 = "<resource>";
-  protected static final String _45 = "<resources>";
-  protected static final String _46 = "<version>";
-  protected static final String _47 = "<version>${tycho-version}</version>";
-  protected static final String _48 = "<version>1.0.0-SNAPSHOT</version>";
-  protected static final String _49 = "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"";
-  protected static final String _50 = "xmlns=\"http://maven.apache.org/POM/4.0.0\"";
-  protected static final String _51 = "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">";
+  protected static final String _43 = "<resource>";
+  protected static final String _44 = "<resources>";
+  protected static final String _45 = "<version>";
+  protected static final String _46 = "<version>${tycho-version}</version>";
+  protected static final String _47 = "<version>1.0.0-SNAPSHOT</version>";
+  protected static final String _48 = "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"";
+  protected static final String _49 = "xmlns=\"http://maven.apache.org/POM/4.0.0\"";
+  protected static final String _50 = "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">";
   protected final String NL_1 = NL + "  ";
   protected final String NL_2 = NL + "    ";
   protected final String NL_3 = NL + "      ";
@@ -82,15 +81,15 @@ public class POMXML
   protected final String NL_5 = NL + "          ";
   protected final String NL_6 = NL + "            ";
   protected final String NL_7 = NL + "              ";
-  protected final String _52 = NL + _1 + NL + _41 + NL_2 + _50 + NL_2 + _49 + NL_2 + _51 + NL_1 + _35 + NL + NL_1 + _38 + NL_2 + _31;
-  protected final String _53 = _12 + NL_2 + _23;
-  protected final String _54 = _4 + NL_2 + _48 + NL_2 + _42;
-  protected final String _55 = _3 + NL_1 + _15 + NL + NL_1 + _31;
-  protected final String _56 = _12 + NL_1 + _23;
-  protected final String _57 = _7 + NL_1 + _46;
-  protected final String _58 = _2 + NL_1 + _37 + NL + NL_1 + _25 + NL_2 + _45 + NL_3 + _44 + NL_4 + _27 + NL_4 + _30 + NL_4 + _34 + NL_5 + _33 + NL_4 + _13 + NL_3 + _19 + NL_2 + _20 + NL_2 + _40 + NL_3 + _39 + NL_4 + _32 + NL_4 + _24 + NL_4 + _47 + NL_4 + _26 + NL_5 + _43 + NL_5 + _29 + NL_6 + _28 + NL_7 + _36;
-  protected final String _59 = _14 + NL_7 + _22;
-  protected final String _60 = _6 + NL_6 + _10 + NL_5 + _11 + NL_4 + _9 + NL_3 + _16 + NL_2 + _17 + NL_1 + _8 + NL + NL + _18;
+  protected final String _51 = NL + _1 + NL + _41 + NL_2 + _49 + NL_2 + _48 + NL_2 + _50 + NL_1 + _35 + NL + NL_1 + _38 + NL_2 + _31;
+  protected final String _52 = _12 + NL_2 + _23;
+  protected final String _53 = _4 + NL_2 + _47 + NL_2 + _42;
+  protected final String _54 = _3 + NL_1 + _15 + NL + NL_1 + _31;
+  protected final String _55 = _12 + NL_1 + _23;
+  protected final String _56 = _7 + NL_1 + _45;
+  protected final String _57 = _2 + NL_1 + _37 + NL + NL_1 + _25 + NL_2 + _44 + NL_3 + _43 + NL_4 + _27 + NL_4 + _30 + NL_4 + _34 + NL_5 + _33 + NL_4 + _13 + NL_3 + _19 + NL_2 + _20 + NL_2 + _40 + NL_3 + _39 + NL_4 + _32 + NL_4 + _24 + NL_4 + _46 + NL_4 + _26 + NL_5 + _29 + NL_6 + _28 + NL_7 + _36;
+  protected final String _58 = _14 + NL_7 + _22;
+  protected final String _59 = _6 + NL_6 + _10 + NL_5 + _11 + NL_4 + _9 + NL_3 + _16 + NL_2 + _17 + NL_1 + _8 + NL + NL + _18;
 
   public String generate(Object argument)
   {
@@ -109,6 +108,8 @@ public class POMXML
     builder.append(_5);
     builder.append(NL);
     builder.append(org.eclipse.justj.codegen.model.util.Generator.getCopyright(argument, "", NL));
+    builder.append(_51);
+    builder.append(modelName);
     builder.append(_52);
     builder.append(modelName);
     builder.append(_53);
@@ -116,16 +117,14 @@ public class POMXML
     builder.append(_54);
     builder.append(modelName);
     builder.append(_55);
-    builder.append(modelName);
-    builder.append(_56);
     builder.append(fullName);
-    builder.append(_57);
+    builder.append(_56);
     builder.append(version);
-    builder.append(_58);
+    builder.append(_57);
     builder.append(os);
-    builder.append(_59);
+    builder.append(_58);
     builder.append(arch);
-    builder.append(_60);
+    builder.append(_59);
     builder.append(NL);
     return builder.toString();
   }
